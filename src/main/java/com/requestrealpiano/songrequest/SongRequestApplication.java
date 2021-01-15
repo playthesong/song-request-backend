e7734a32465b16a7ed64d8e590e2b2de798d172c
ragdoll <museopkim0214@gmail.com>
2021-01-16 01:56:54 +0900

    [#3] Chore: 프로젝트 생성
    프로젝트 생성
    - SpringBoot 2.3.8
    - Spring Web
    - Spring Data JPA
    - MySQL Driver
    - H2 Driver
    - Spring Security
    - OAuth2 Client
    - Spring Rest Docs
    - Lombok

---
+++ src/main/java/com/requestrealpiano/songrequest/SongRequestApplication.java
@@ -0,0 +1,12 @@
+package com.requestrealpiano.songrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongRequestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/requestrealpiano/songrequest/SongRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.requestrealpiano.songrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongRequestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
