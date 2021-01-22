6f10762741fae38f8758e0a329de00e8beda0fd4
ragdoll <museopkim0214@gmail.com>
2021-01-22 23:02:07 +0900

    [#6] Fix: ManiaDbArtist DTO 필드 누락 수정
    ManiaDbArtist
    - name 필드 추가

--- src/main/java/com/requestrealpiano/songrequest/domain/song/searchapi/maniadb/response/inner/ManiaDbArtist.java
+++ src/main/java/com/requestrealpiano/songrequest/domain/song/searchapi/maniadb/response/inner/ManiaDbArtist.java
@@ -1,4 +1,14 @@
 package com.requestrealpiano.songrequest.domain.song.searchapi.maniadb.response.inner;
 
+import lombok.Getter;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Getter
+@XmlRootElement(name = "manidb:artist")
 public class ManiaDbArtist {
+
+    @XmlElement(name = "name")
+    private String name;
 }
