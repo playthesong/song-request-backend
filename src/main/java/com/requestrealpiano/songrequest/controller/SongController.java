9469824df5886c93707745c22347ab6054574944
ragdoll <museopkim0214@gmail.com>
2021-01-24 01:31:15 +0900

    [#6] Feat: ManiaDB 데이터 검색 API 추가 완료
    SongController
    - searchManiaDbApi()

---
+++ src/main/java/com/requestrealpiano/songrequest/controller/SongController.java
@@ -0,0 +1,26 @@
+package com.requestrealpiano.songrequest.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.requestrealpiano.songrequest.domain.song.searchapi.maniadb.response.ManiaDbResponse;
+import com.requestrealpiano.songrequest.global.response.ApiResponse;
+import com.requestrealpiano.songrequest.service.SongService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/songs")
+public class SongController {
+
+    private final SongService songService;
+
+    @GetMapping("/search-api/k-pop")
+    public ApiResponse<ManiaDbResponse> searchManiaDbApi(@RequestParam String artist,
+                                                         @RequestParam String title) throws JsonProcessingException {
+        ManiaDbResponse maniaDbResponse = songService.searchManiaDb(artist, title);
+        return ApiResponse.OK(maniaDbResponse);
+    }
+}
