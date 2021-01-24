fc5725f4b0bff072e05d0a9f24c1722c2f883546
ragdoll <museopkim0214@gmail.com>
2021-01-25 00:53:28 +0900

    [#6] Test: LastFm Track 데이터 (LastFmResponse) 테스트 추가
    LastFmResponseTest

---
+++ src/test/java/com/requestrealpiano/songrequest/domain/song/searchapi/lastfm/LastFmResponseTest.java
@@ -0,0 +1,22 @@
+//package com.requestrealpiano.songrequest.domain.song.searchapi.lastfm;
+//
+//import com.requestrealpiano.songrequest.domain.song.searchapi.lastfm.response.inner.LastFmTrack;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class LastFmResponseTest {
+//
+//    @Test
+//    @DisplayName("LastFm 응답 데이터를 바탕으로 최종 검색 결과 DTO를 생성하는 테스트")
+//    void create_new_lastfm_response() {
+//
+//    }
+//
+//    private List<LastFmTrack> createMockTracks() {
+//        LastFmTrack firstTrack = new LastFmTrack("title", "artist", "url");
+//    }
+//}
