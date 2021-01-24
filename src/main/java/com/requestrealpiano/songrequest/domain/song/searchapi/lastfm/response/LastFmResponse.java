5b38e16ee4c83a6dc89489339b248e5aa2b260c2
ragdoll <museopkim0214@gmail.com>
2021-01-24 18:39:13 +0900

    [#6] Feat: LastFm 검색 결과 데이터 DTO 추가
    LastFmResponse

---
+++ src/main/java/com/requestrealpiano/songrequest/domain/song/searchapi/lastfm/response/LastFmResponse.java
@@ -0,0 +1,27 @@
+package com.requestrealpiano.songrequest.domain.song.searchapi.lastfm.response;
+
+import com.requestrealpiano.songrequest.domain.song.searchapi.lastfm.response.inner.LastFmTrack;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class LastFmResponse {
+
+    private int totalCount;
+    private List<LastFmTrack> tracks;
+
+    @Builder
+    private LastFmResponse(int totalCount, List<LastFmTrack> tracks) {
+        this.totalCount = totalCount;
+        this.tracks = tracks;
+    }
+
+    public static LastFmResponse of(int totalCount, List<LastFmTrack> tracks) {
+        return LastFmResponse.builder()
+                             .totalCount(totalCount)
+                             .tracks(tracks)
+                             .build();
+    }
+}
