47791111ca85219b2043a7eefaa489950e05636c
ragdoll <museopkim0214@gmail.com>
2021-01-26 14:36:47 +0900

    [#9] Refactor: SearchApiService 테스트 코드 수정
    SearchApiServiceTest
    - @SpringBootTest로 필요한 의존성들을 빈으로 주입하여 통합 테스트 방식으로 결정
    - 검색 기능은 본래가 음원 검색 API에 온전히 의존하는 기능이므로, 테스트는 RestTemplate을 Mocking 하여 별도의 유닛 테스트로 만들 필요가 없다고 판단 했습니다.
    -  검색 API가 제공하는 데이터의 내용은 추후에 달라질 수 있으므로, isEqualTo() 보다는 conatins()와 같은 유연한 Assertions를 사용하도록 했습니다.

--- src/test/java/com/requestrealpiano/songrequest/service/SearchApiServiceTest.java
+++ src/test/java/com/requestrealpiano/songrequest/service/SearchApiServiceTest.java
@@ -27,7 +27,8 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-@SpringBootTest(classes = {SearchApiService.class, ManiaDbRestClient.class, LastFmRestClient.class, JsonTranslator.class, XmlTranslator.class, SearchApiServiceTest.TestConfiguration.class})
+@SpringBootTest(classes = {SearchApiService.class, ManiaDbRestClient.class, LastFmRestClient.class,
+                           JsonTranslator.class, XmlTranslator.class, SearchApiServiceTest.TestConfiguration.class})
 class SearchApiServiceTest {
 
     @Autowired
@@ -49,36 +50,9 @@ class SearchApiServiceTest {
     public static class TestConfiguration { }
 
     @ParameterizedTest
-    @CsvSource("김동률, 감사, http://i.maniadb.com/images/album/148/148138_1_f.jpg, " +
-               "10")
+    @CsvSource("김동률, 감사, 10")
     @DisplayName("ManiaDB 검색 결과 반환 테스트")
-    void search_maniaDb_response(String artist, String title, String imageUrl, int totalCount) throws IOException {
-        // given
-        ManiaDbTrack testTrack = ManiaDbTrack.builder()
-                                             .artist(artist)
-                                             .title(title)
-                                             .imageUrl(imageUrl)
-                                             .build();
-
-        /*
-          TODO:
-            각 의존성들을 Mocking 한 뒤 처리하려 했으나 정적 팩토리 메서드로 생성하는 과정을
-            Mocktito.when().thenReturn() 으로 처리하면 아래와 같은 에러 발생하였다.
-            우선 필요한 실제 빈을 주입하여 테스트하기로 하였고, 추후 기능 구현이 끝난 뒤 개선 방법이 없을 지 다시 고민 해보려 한다.
-
-            org.mockito.exceptions.misusing.WrongTypeOfReturnValue:
-            ManiaDbRestClient$MockitoMock$1303731471$MockitoMock$830141523 cannot be returned by getVersion()
-            getVersion() should return String
-
-            ***
-            If you're unsure why you're getting above error read on.
-            Due to the nature of the syntax above problem might occur because:
-            1. This exception *might* occur in wrongly written multi-threaded tests.
-               Please refer to Mockito FAQ on limitations of concurrency testing.
-            2. A spy is stubbed using when(spy.foo()).then() syntax. It is safer to stub spies -
-               - with doReturn|Throw() family of methods. More in javadocs for Mockito.spy() method.
-         */
-
+    void search_maniaDb_response(String artist, String title, int totalCount) throws IOException {
         // when
         ManiaDbResponse maniaDbResponse = searchApiService.searchManiaDbResponse(artist, title);
         List<ManiaDbTrack> tracks = maniaDbResponse.getTracks();
@@ -89,9 +63,8 @@ class SearchApiServiceTest {
         // then
         assertAll(
                 () -> assertThat(maniaDbResponse.getTotalCount()).isEqualTo(totalCount),
-                () -> assertThat(track.getTitle()).isEqualTo(testTrack.getTitle()),
-                () -> assertThat(track.getArtist()).isEqualTo(testTrack.getArtist()),
-                () -> assertThat(track.getImageUrl()).isEqualTo(testTrack.getImageUrl())
+                () -> assertThat(track.getArtist()).contains(artist),
+                () -> assertThat(track.getTitle()).contains(title)
         );
     }
 
