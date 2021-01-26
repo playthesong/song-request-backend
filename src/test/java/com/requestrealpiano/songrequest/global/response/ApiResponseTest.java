bbe335495699fd498fbf1d2e58911007eaa07dab
ragdoll <museopkim0214@gmail.com>
2021-01-27 01:09:57 +0900

    [#9] Refactor: 표준 응답 객체 (ApiResponse) 테스트 분리
    ApiResponseTest
    - OK, CREATED, UPDATED, DELETED 각 응답에 대한 테스트를 메서드로 분리

--- src/test/java/com/requestrealpiano/songrequest/global/response/ApiResponseTest.java
+++ src/test/java/com/requestrealpiano/songrequest/global/response/ApiResponseTest.java
@@ -9,21 +9,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class ApiResponseTest {
 
     @Test
-    @DisplayName("표준 응답 객체 생성 테스트")
+    @DisplayName("표준 응답 객체 생성 테스트 - OK")
     void create_new_api_response_OK() {
         // given
         String testMessage = "Test Message";
         String OK_MESSAGE = "OK";
-        String CREATED_MESSAGE = "CREATED";
-        String UPDATED_MESSAGE = "UPDATED";
-        String DELETED_MESSAGE = "DELETED";
-
 
         // when
         ApiResponse<String> ApiResponseOK = ApiResponse.OK(testMessage);
-        ApiResponse<Void> ApiResponseCREATED = ApiResponse.CREATED();
-        ApiResponse<Void> ApiResponseUPDATED = ApiResponse.UPDATED();
-        ApiResponse<Void> ApiResponseDELETED = ApiResponse.DELETED();
 
         // then
         assertAll(
@@ -31,24 +24,56 @@ class ApiResponseTest {
                 () -> assertThat(ApiResponseOK.getStatusMessage()).isEqualTo(OK_MESSAGE),
                 () -> assertThat(ApiResponseOK.getData()).isEqualTo(testMessage)
         );
+    }
 
+    @Test
+    @DisplayName("표준 응답 객체 생성 테스트 - CREATED")
+    void create_new_api_response_CREATED() {
+        // given
+        String CREATED_MESSAGE = "CREATED";
+
+        // when
+        ApiResponse<Void> ApiResponseCREATED = ApiResponse.CREATED();
+
+        // then
         assertAll(
                 () -> assertThat(ApiResponseCREATED.isSuccess()).isTrue(),
                 () -> assertThat(ApiResponseCREATED.getStatusMessage()).isEqualTo(CREATED_MESSAGE),
                 () -> assertThat(ApiResponseCREATED.getData()).isNull()
         );
+    }
 
+    @Test
+    @DisplayName("표준 응답 객체 생성 테스트 - UPDATED")
+    void create_new_api_response_UPDATED() {
+        // given
+        String UPDATED_MESSAGE = "UPDATED";
+
+        // when
+        ApiResponse<Void> ApiResponseUPDATED = ApiResponse.UPDATED();
+
+        // then
         assertAll(
                 () -> assertThat(ApiResponseUPDATED.isSuccess()).isTrue(),
                 () -> assertThat(ApiResponseUPDATED.getStatusMessage()).isEqualTo(UPDATED_MESSAGE),
                 () -> assertThat(ApiResponseUPDATED.getData()).isNull()
         );
+    }
 
+    @Test
+    @DisplayName("표준 응답 객체 생성 테스트 - DELETED")
+    void create_new_api_response_DELETED() {
+        // given
+        String DELETED_MESSAGE = "DELETED";
+
+        // when
+        ApiResponse<Void> ApiResponseDELETED = ApiResponse.DELETED();
+
+        // then
         assertAll(
                 () -> assertThat(ApiResponseDELETED.isSuccess()).isTrue(),
                 () -> assertThat(ApiResponseDELETED.getStatusMessage()).isEqualTo(DELETED_MESSAGE),
                 () -> assertThat(ApiResponseDELETED.getData()).isNull()
         );
     }
-
 }
