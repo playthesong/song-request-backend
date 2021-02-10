aca103ce8fe5a0af9d66b916ac068f5440d6a0ab
ragdoll <museopkim0214@gmail.com>
2021-02-10 16:03:24 +0900

    [#14] Fix: 불필요한 Enum value 삭제 (Role, RequestStatus)
    Role,
    RequestStatus
    - 프론트에서 Enum 기본 값을 받아 후처리 하도록 변경됨에 따라 삭제
    
    RoleTest
    - 불필요한 테스트 삭제

--- src/test/java/com/requestrealpiano/songrequest/domain/account/RoleTest.java
+++ src/test/java/com/requestrealpiano/songrequest/domain/account/RoleTest.java
@@ -8,36 +8,4 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class RoleTest {
-
-    @ParameterizedTest
-    @CsvSource({"ADMIN, MEMBER, GUEST, ROLE_ADMIN, ROLE_MEMBER, ROLE_GUEST, 관리자, 회원, 권한 승인 대기자"})
-    @DisplayName("Role의 각 속성들을 테스트 한다.")
-    void compare_role_properties(String adminName, String memberName, String guestName,
-                                 String adminValue, String memberValue, String guestValue,
-                                 String adminDescription, String memberDescription, String guestDescription) {
-        // when
-        Role admin = Role.ADMIN;
-        Role member = Role.MEMBER;
-        Role guest = Role.GUEST;
-
-        // then
-        assertAll(
-                () -> assertThat(admin.getKey()).isEqualTo(adminName),
-                () -> assertThat(admin.getValue()).isEqualTo(adminValue),
-                () -> assertThat(admin.getDescription()).isEqualTo(adminDescription)
-        );
-
-        assertAll(
-                () -> assertThat(member.getKey()).isEqualTo(memberName),
-                () -> assertThat(member.getValue()).isEqualTo(memberValue),
-                () -> assertThat(member.getDescription()).isEqualTo(memberDescription)
-        );
-
-        assertAll(
-                () -> assertThat(guest.getKey()).isEqualTo(guestName),
-                () -> assertThat(guest.getValue()).isEqualTo(guestValue),
-                () -> assertThat(guest.getDescription()).isEqualTo(guestDescription)
-        );
-
-    }
 }
