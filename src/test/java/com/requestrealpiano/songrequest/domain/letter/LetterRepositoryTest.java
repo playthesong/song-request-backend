b31e3e9a3d40ad6432146ba5a518fb956dbba56a
ragdoll <museopkim0214@gmail.com>
2021-02-10 21:10:56 +0900

    [#14] Refactor: LetterRepositoryTest ParameterizedTest 로 변경
    ParameterizedTest 로 변경하여 하드 코딩 및 주석 제거

--- src/test/java/com/requestrealpiano/songrequest/domain/letter/LetterRepositoryTest.java
+++ src/test/java/com/requestrealpiano/songrequest/domain/letter/LetterRepositoryTest.java
@@ -2,13 +2,15 @@ package com.requestrealpiano.songrequest.domain.letter;
 
 import com.requestrealpiano.songrequest.testconfig.BaseRepositoryTest;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,9 +19,11 @@ class LetterRepositoryTest extends BaseRepositoryTest {
     @Autowired
     LetterRepository letterRepository;
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("findAllLettersParameters")
     @DisplayName("Letter 데이터를 DB에서 조회한다.")
-    void find_all_letters() {
+    void find_all_letters(int first, int second, int third,
+                          Long firstLetterId, Long secondLetterId, Long thirdLetterId) {
         // given
         List<Letter> letters = createMockLetters();
 
@@ -27,20 +31,24 @@ class LetterRepositoryTest extends BaseRepositoryTest {
         letterRepository.saveAll(letters);
         List<Letter> savedLetters = letterRepository.findAll();
 
-        /* 0, 1, 2 - List Index */
-        Letter firstLetter = letters.get(0);
-        Letter secondLetter = letters.get(1);
-        Letter thirdLetter = letters.get(2);
+        Letter firstLetter = letters.get(first);
+        Letter secondLetter = letters.get(second);
+        Letter thirdLetter = letters.get(third);
 
         // then
         assertThat(savedLetters).hasSize(letters.size());
         assertThat(savedLetters.stream().filter(letter -> Optional.ofNullable(letter.getId()).isPresent())
                                .count()).isEqualTo(letters.size());
 
-        /* 1L, 2L, 3L - Auto Increment 생성 ID */
-        assertThat(firstLetter.getId()).isEqualTo(1L);
-        assertThat(secondLetter.getId()).isEqualTo(2L);
-        assertThat(thirdLetter.getId()).isEqualTo(3L);
+        assertThat(firstLetter.getId()).isEqualTo(firstLetterId);
+        assertThat(secondLetter.getId()).isEqualTo(secondLetterId);
+        assertThat(thirdLetter.getId()).isEqualTo(thirdLetterId);
+    }
+
+    private static Stream<Arguments> findAllLettersParameters() {
+        return Stream.of(
+                Arguments.of(0, 1, 2, 1L, 2L, 3L)
+        );
     }
 
     private List<Letter> createMockLetters() {
