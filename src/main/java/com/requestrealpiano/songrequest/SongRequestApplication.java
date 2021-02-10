f5a57bea049afd0bfa7f4d77186ea39876a9edc6
ragdoll <museopkim0214@gmail.com>
2021-02-10 17:35:10 +0900

    [#14] Refactor: Configuration properties 불변 객체로 변경
    변경 가능성 있는 Setter 기반 바인딩을 생성자 기반 바인딩으로 수정
    
    LastFmProperties,
    ManiaDbProperties

--- src/main/java/com/requestrealpiano/songrequest/SongRequestApplication.java
+++ src/main/java/com/requestrealpiano/songrequest/SongRequestApplication.java
@@ -1,8 +1,12 @@
 package com.requestrealpiano.songrequest;
 
+import com.requestrealpiano.songrequest.config.searchapi.LastFmProperties;
+import com.requestrealpiano.songrequest.config.searchapi.ManiaDbProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(value = {ManiaDbProperties.class, LastFmProperties.class})
 @SpringBootApplication
 public class SongRequestApplication {
 
--- src/main/java/com/requestrealpiano/songrequest/config/searchapi/ManiaDbProperties.java
+++ src/main/java/com/requestrealpiano/songrequest/config/searchapi/ManiaDbProperties.java
@@ -1,19 +1,21 @@
 package com.requestrealpiano.songrequest.config.searchapi;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Getter
-@Setter
-@Component
+@ConstructorBinding
 @ConfigurationProperties(prefix = "maniadb")
 public class ManiaDbProperties {
 
-    private String url;
-    private String display;
-    private String method;
-    private String apiKey;
-    private String version;
+    private final String url;
+    private final String display;
+    private final String method;
+    private final String apiKey;
+    private final String version;
 }
