5639a3467966214ccc89e3ebffd3d867bb6ef7b2
ragdoll <museopkim0214@gmail.com>
2021-02-13 19:55:51 +0900

    [#14] Feat: Default TimeZone 설정
    KST 설정

--- src/main/java/com/requestrealpiano/songrequest/SongRequestApplication.java
+++ src/main/java/com/requestrealpiano/songrequest/SongRequestApplication.java
@@ -6,9 +6,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableConfigurationProperties(value = {ManiaDbProperties.class, LastFmProperties.class})
 @SpringBootApplication
 public class SongRequestApplication {
+
+    @PostConstruct
+    public void setTimeZone() {
+        String SEOUL = "Asia/Seoul";
+        TimeZone.setDefault(TimeZone.getTimeZone(SEOUL));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SongRequestApplication.class, args);
