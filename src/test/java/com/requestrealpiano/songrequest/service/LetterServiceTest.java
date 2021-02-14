1a6f0efc4f8a7ddf058ed552eee375ed2620904e
ragdoll <museopkim0214@gmail.com>
2021-02-14 20:39:31 +0900

    [#16] Feat: LetterService - Letter 상세조회 추가
    LetterService
    - LetterNotFoundException 예외에 관한 상세한 정의는 추후 예정
    
    LetterServiceTest
    - 성공 테스트 및 예외 발생 테스트 추가

--- src/test/java/com/requestrealpiano/songrequest/service/LetterServiceTest.java
+++ src/test/java/com/requestrealpiano/songrequest/service/LetterServiceTest.java
@@ -6,18 +6,24 @@ import com.requestrealpiano.songrequest.domain.letter.LetterRepository;
 import com.requestrealpiano.songrequest.domain.letter.RequestStatus;
 import com.requestrealpiano.songrequest.domain.letter.dto.response.LetterResponse;
 import com.requestrealpiano.songrequest.domain.song.Song;
+import com.requestrealpiano.songrequest.global.error.LetterNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -67,4 +73,46 @@ class LetterServiceTest {
                 .build();
         return Arrays.asList(firstLetter, secondLetter, thirdLetter);
     }
+
+    @DisplayName("정상적인 ID로부터 Letter를 조회하는 테스트")
+    @ParameterizedTest
+    @MethodSource("findLetterByValidIdParameters")
+    void find_letter_by_valid_id(Long validId, String songStory, RequestStatus requestStatus) {
+        // given
+        Letter letter = Letter.builder()
+                              .songStory(songStory)
+                              .requestStatus(requestStatus)
+                              .account(Account.builder().build())
+                              .song(Song.builder().build())
+                              .build();
+        LetterResponse testResponse = LetterResponse.from(letter);
+
+        // when
+        when(letterRepository.findById(validId)).thenReturn(Optional.of(letter));
+        LetterResponse letterResponse = letterService.findLetter(validId);
+
+        // then
+        assertThat(letterResponse.getSongStory()).isEqualTo(testResponse.getSongStory());
+        assertThatCode(() -> letterService.findLetter(validId)).doesNotThrowAnyException();
+    }
+
+    private static Stream<Arguments> findLetterByValidIdParameters() {
+        return Stream.of(
+                Arguments.of(1L, "Song Story", RequestStatus.WAITING)
+        );
+    }
+
+    @DisplayName("LetterNotFoundException - 존재하지 않는 ID 값으로 Letter 를 조회할 때 예외가 발생하는 테스트")
+    @Test
+    void find_letter_by_invalid_id() {
+        // given
+        Long invalidId = 1234567L;
+
+        // when
+        when(letterRepository.findById(invalidId)).thenReturn(Optional.empty());
+
+        // then
+        assertThatThrownBy(() -> letterService.findLetter(invalidId)).isInstanceOf(LetterNotFoundException.class);
+    }
+
 }
