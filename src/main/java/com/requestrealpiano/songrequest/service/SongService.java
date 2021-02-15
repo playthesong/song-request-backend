0136f3ba3a196f162df2045120bb6ed2107be02c
ragdoll <museopkim0214@gmail.com>
2021-02-15 17:03:06 +0900

    [#7] Feat: Letter 등록 시 요청된 Song을 DB에서 먼저 조회한 뒤 반환하는 로직 추가
    SongService
    - 동일한 데이터의 중복 삽입을 방지하기 위해 먼저 DB에서 Song을 조회 후 저장 혹은 반환 처리
    
    SongServiceTest
    - 해당 테스트 추가

--- src/main/java/com/requestrealpiano/songrequest/service/SongService.java
+++ src/main/java/com/requestrealpiano/songrequest/service/SongService.java
@@ -1,6 +1,9 @@
 package com.requestrealpiano.songrequest.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.requestrealpiano.songrequest.domain.letter.dto.request.inner.SongRequest;
+import com.requestrealpiano.songrequest.domain.song.Song;
+import com.requestrealpiano.songrequest.domain.song.SongRepository;
 import com.requestrealpiano.songrequest.domain.song.searchapi.response.SearchApiResponse;
 import com.requestrealpiano.songrequest.service.searchapi.SearchApiService;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +13,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class SongService {
 
+    private final SongRepository songRepository;
     private final SearchApiService searchApiService;
+
+    public Song findSongByRequest(SongRequest songRequest) {
+        return songRepository.findBySongTitleContainingIgnoreCaseAndArtistIgnoreCase(songRequest.getTitle(), songRequest.getArtist())
+                             .orElseGet(() -> {
+                                 Song song = Song.from(songRequest);
+                                 return songRepository.save(song);
+                             });
+    }
 
     public SearchApiResponse searchSong(String artist, String title) throws JsonProcessingException {
         return searchApiService.requestSearchApiResponse(artist, title);
