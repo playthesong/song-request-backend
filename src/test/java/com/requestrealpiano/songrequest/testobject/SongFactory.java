a8e169d25e97c3258f1ab20d6b39cfb59de9b610
ragdoll <museopkim0214@gmail.com>
2021-02-16 04:27:01 +0900

    [#19] Refactor: Song 도메인 테스트 리팩토링
    SongRepositoryTest
    - 테스트 객체 정적 메서드로부터 생성하도록 변경
    - 케이스별 테스트를 위해 세분화
    - 테스트 객체 내부 값에 의존하지 않도록 String 메서드로 artist, title 값 변경
    
    SongTest
    - 정적 메서드로부터 테스트 객체 생성
    - 불필요한 빌더 테스트 삭제
    
    SongFactory
    - createList()
    - createRequest() : 다른 곳에서도 중복 사용 되고 있으므로 SongRequest 객체 생성 메서드 별도로 추가

--- src/test/java/com/requestrealpiano/songrequest/testobject/SongFactory.java
+++ src/test/java/com/requestrealpiano/songrequest/testobject/SongFactory.java
@@ -1,6 +1,10 @@
 package com.requestrealpiano.songrequest.testobject;
 
+import com.requestrealpiano.songrequest.domain.letter.dto.request.inner.SongRequest;
+import com.requestrealpiano.songrequest.domain.letter.dto.request.inner.SongRequestBuilder;
 import com.requestrealpiano.songrequest.domain.song.Song;
+
+import java.util.List;
 
 public class SongFactory {
 
@@ -13,5 +17,32 @@ public class SongFactory {
                    .requestCount(1)
                    .build();
 
+    }
+
+    public static List<Song> createList() {
+        Song firstSong = Song.builder()
+                             .songTitle("Wonderwall")
+                             .artist("Oasis")
+                             .imageUrl("http://oasis.img")
+                             .build();
+        Song secondSong = Song.builder()
+                              .songTitle("Hey Jude")
+                              .artist("The Beatles")
+                              .imageUrl("http://thebeatles.img")
+                              .build();
+        Song thirdSong = Song.builder()
+                             .songTitle("Something Just Like This")
+                             .artist("Coldplay")
+                             .imageUrl("http://coldplay.img")
+                             .build();
+        return List.of(firstSong, secondSong, thirdSong);
+    }
+
+    public static SongRequest createRequest() {
+        return SongRequestBuilder.newBuilder()
+                                 .title("New Title")
+                                 .artist("New Artist")
+                                 .imageUrl("New Image URL")
+                                 .build();
     }
 }
