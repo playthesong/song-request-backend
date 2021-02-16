dde5e8db42796e9bba2b6986f6f767bf1b45731e
ragdoll <museopkim0214@gmail.com>
2021-02-16 18:04:54 +0900

    [#19] Feat: LetterFactory - account, song을 인자로 받아 생성하는 메서드 추가
    `@DataJpaTest`에서 만약 다른 엔티티를 필드로 가지고 있는 엔티티를 save() 할 경우,
     `org.hibernate.TransientPropertyValueException` 가 발생한다.
     따라서 `DataJpaTest` 에서는 의존관계가 없는 테스트 객체를 사용하고,
     테스트 엔티티 객체가 특정 엔티티를 필요로 할 경우 이를 파라미터로 받아 사용할 수 있도록 별도의 메서드로 분리하였다.
    
    LetterFactory
    - createLettersOf()

--- src/test/java/com/requestrealpiano/songrequest/testobject/LetterFactory.java
+++ src/test/java/com/requestrealpiano/songrequest/testobject/LetterFactory.java
@@ -1,7 +1,9 @@
 package com.requestrealpiano.songrequest.testobject;
 
+import com.requestrealpiano.songrequest.domain.account.Account;
 import com.requestrealpiano.songrequest.domain.letter.Letter;
 import com.requestrealpiano.songrequest.domain.letter.RequestStatus;
+import com.requestrealpiano.songrequest.domain.song.Song;
 
 import java.util.Arrays;
 import java.util.List;
@@ -29,6 +31,28 @@ public class LetterFactory {
         Letter thirdLetter = Letter.builder()
                                    .songStory("Song Story 3")
                                    .requestStatus(RequestStatus.PENDING)
+                                   .build();
+        return Arrays.asList(firstLetter, secondLetter, thirdLetter);
+    }
+
+    public static List<Letter> createLettersOf(Account account, Song song) {
+        Letter firstLetter = Letter.builder()
+                                .songStory("Song Story 1")
+                                .requestStatus(RequestStatus.WAITING)
+                                .account(account)
+                                .song(song)
+                                .build();
+        Letter secondLetter = Letter.builder()
+                                    .songStory("Song Story 2")
+                                    .requestStatus(RequestStatus.DONE)
+                                    .account(account)
+                                    .song(song)
+                                    .build();
+        Letter thirdLetter = Letter.builder()
+                                   .songStory("Song Story 3")
+                                   .requestStatus(RequestStatus.PENDING)
+                                   .account(account)
+                                   .song(song)
                                    .build();
         return Arrays.asList(firstLetter, secondLetter, thirdLetter);
     }
