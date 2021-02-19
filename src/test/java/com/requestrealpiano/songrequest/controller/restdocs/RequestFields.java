223281cd0d7c15a0e7ec57a4179710ad77dca4b8
ragdoll <museopkim0214@gmail.com>
2021-02-19 21:24:37 +0900

    [#21] Feat: Create letter field 제한 정보 추가

--- src/test/java/com/requestrealpiano/songrequest/controller/restdocs/RequestFields.java
+++ src/test/java/com/requestrealpiano/songrequest/controller/restdocs/RequestFields.java
@@ -11,10 +11,10 @@ public class RequestFields {
 
     public static List<FieldDescriptor> createLetter() {
         return List.of(
-                fieldWithPath("songStory").type(JsonFieldType.STRING).description("신청곡 사연 내용"),
+                fieldWithPath("songStory").type(JsonFieldType.STRING).description("신청곡 사연 내용 (500자 이하)"),
                 fieldWithPath("song").type(JsonFieldType.OBJECT).description("신청곡 정보"),
-                fieldWithPath("song.title").type(JsonFieldType.STRING).description("신청곡 제목"),
-                fieldWithPath("song.artist").type(JsonFieldType.STRING).description("아티스트"),
+                fieldWithPath("song.title").type(JsonFieldType.STRING).description("신청곡 제목 (30자 이하)"),
+                fieldWithPath("song.artist").type(JsonFieldType.STRING).description("아티스트 (30자 이하)"),
                 fieldWithPath("song.imageUrl").type(JsonFieldType.STRING).description("앨범 이미지"),
                 fieldWithPath("accountId").type(JsonFieldType.NUMBER).description("신청자 ID")
         );
