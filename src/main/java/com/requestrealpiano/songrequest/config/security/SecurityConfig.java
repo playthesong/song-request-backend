83e42a2af8a3493426e7600a8e7f691ac1ddf949
ragdoll <museopkim0214@gmail.com>
2021-02-25 23:16:30 +0900

    [#41] Feat: 기본 Google OAuth 인증 로직 추가
    CustomOAuth2UserService
    - DB에서 Email로 먼저 조회 후 현재 계정이 존재 할 경우 Profile 업데이트, 아닐 경우 저장
    
    SecurityConfig
    - 세션 기반으로 수동 테스트 하기 위해 무효화 코드 임시 주석 처리
    
    OAuthAttributes
    - Google OAuth 속성 정의

--- src/main/java/com/requestrealpiano/songrequest/config/security/SecurityConfig.java
+++ src/main/java/com/requestrealpiano/songrequest/config/security/SecurityConfig.java
@@ -1,21 +1,36 @@
 package com.requestrealpiano.songrequest.config.security;
 
+import com.requestrealpiano.songrequest.config.security.oauth.CustomOAuth2UserService;
+import com.requestrealpiano.songrequest.domain.account.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final CustomOAuth2UserService customOAuth2UserService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/**").permitAll()
-            .anyRequest().authenticated();
-
         http.csrf()
             .disable();
+
+        http.authorizeRequests()
+            .antMatchers("/api/**").hasAnyRole(Role.MEMBER.getKey(), Role.ADMIN.getKey())
+            .anyRequest().authenticated();
+
+//        http.sessionManagement()
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
     }
 }
