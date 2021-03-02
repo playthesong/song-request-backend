3fd3966c1ab5c0fbc44ea61142710f7a60324b32
ragdoll <museopkim0214@gmail.com>
2021-03-02 23:39:45 +0900

    [#41] Feat: Security CORS 설정 적용
    Security가 적용 된 경우 아무런 인증 쿠키가 없다면 pre-flight request 과정에서 모든 요청이 차단 된다. 따라서 시큐리티에서 별도의 CORS 설정이 필요하다.
    
    <공식문서 본문>
    Spring Framework provides first class support for CORS. CORS must be processed before Spring Security because the pre-flight request will not contain any cookies (i.e. the JSESSIONID). If the request does not contain any cookies and Spring Security is first, the request will determine the user is not authenticated (since there are no cookies in the request) and reject it.

--- src/main/java/com/requestrealpiano/songrequest/config/security/SecurityConfig.java
+++ src/main/java/com/requestrealpiano/songrequest/config/security/SecurityConfig.java
@@ -2,13 +2,21 @@ package com.requestrealpiano.songrequest.config.security;
 
 import com.requestrealpiano.songrequest.config.security.oauth.CustomAuthenticationSuccessHandler;
 import com.requestrealpiano.songrequest.config.security.oauth.CustomOAuth2UserService;
-import com.requestrealpiano.songrequest.domain.account.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @RequiredArgsConstructor
 @Configuration
@@ -20,6 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors(withDefaults());
+
         http.csrf().disable()
             .httpBasic().disable();
 
@@ -38,4 +48,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .successHandler(customAuthenticationSuccessHandler);
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
