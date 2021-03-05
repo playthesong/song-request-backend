46b7e97a5ecaa8b35635f9d41c43f35ba9f51644
ragdoll <museopkim0214@gmail.com>
2021-03-05 17:17:17 +0900

    [#41] Fix: Merge conflict

--- src/main/java/com/requestrealpiano/songrequest/config/security/oauth/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/requestrealpiano/songrequest/config/security/oauth/CustomAuthenticationSuccessHandler.java
@@ -12,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
 
 @RequiredArgsConstructor
 @Component
