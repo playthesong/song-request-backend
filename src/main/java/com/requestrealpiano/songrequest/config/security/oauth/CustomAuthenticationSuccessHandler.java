c5db7d7a0f3cdd0468ea5fb3924bacdefa0f3f1b
ragdoll <museopkim0214@gmail.com>
2021-03-06 04:57:52 +0900

    [#41] Fix: 사용하지 않는 import 제거

--- src/main/java/com/requestrealpiano/songrequest/config/security/oauth/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/requestrealpiano/songrequest/config/security/oauth/CustomAuthenticationSuccessHandler.java
@@ -9,11 +9,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URI;
 
 @RequiredArgsConstructor
 @Component
