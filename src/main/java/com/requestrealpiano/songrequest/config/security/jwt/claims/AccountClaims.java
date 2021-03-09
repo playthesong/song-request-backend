464e50c77ec281138ed6a46e059730444c9960eb
ragdoll <museopkim0214@gmail.com>
2021-03-09 18:16:08 +0900

    [#41] Feat: Jwt - AccountClaims 수정 (role, Claims를 통한 생성 추가)
    - 클라이언트에서 Role을 확인 할 수 있도록 추가
    - Jwt 토큰을 파싱하는 과정에서 Claims를 받아서 생성할 수 있도록 수정

--- src/main/java/com/requestrealpiano/songrequest/config/security/jwt/claims/AccountClaims.java
+++ src/main/java/com/requestrealpiano/songrequest/config/security/jwt/claims/AccountClaims.java
@@ -1,41 +1,48 @@
 package com.requestrealpiano.songrequest.config.security.jwt.claims;
 
-import com.requestrealpiano.songrequest.config.security.oauth.OAuthAccount;
 import com.requestrealpiano.songrequest.domain.account.Account;
+import com.requestrealpiano.songrequest.domain.account.Role;
+import io.jsonwebtoken.Claims;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class AccountClaims {
 
-    private final long id;
+    private final Long id;
     private final String name;
     private final String email;
     private final String avatarUrl;
+    private final String role;
 
     @Builder
-    private AccountClaims(long id, String name, String email, String avatarUrl) {
+    private AccountClaims(Long id, String name, String email, String avatarUrl, String role) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.avatarUrl = avatarUrl;
+        this.role = role;
     }
 
     public static AccountClaims from(Account account) {
+        Role role = account.getRole();
+
         return AccountClaims.builder()
                             .id(account.getId())
                             .name(account.getName())
                             .email(account.getEmail())
                             .avatarUrl(account.getAvatarUrl())
+                            .role(role.getKey())
                             .build();
     }
 
-    public static AccountClaims from(OAuthAccount oAuthAccount) {
+    public static AccountClaims from(Claims claims) {
         return AccountClaims.builder()
-                            .id(oAuthAccount.getId())
-                            .name(oAuthAccount.getName())
-                            .email(oAuthAccount.getEmail())
-                            .avatarUrl(oAuthAccount.getAvatarUrl())
+                            .id(claims.get("id", Long.class))
+                            .name(claims.get("name", String.class))
+                            .email(claims.get("email", String.class))
+                            .avatarUrl(claims.get("avatarUrl", String.class))
+                            .role(claims.get("role", String.class))
                             .build();
     }
 }
