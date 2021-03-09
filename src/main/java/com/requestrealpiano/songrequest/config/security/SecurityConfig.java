bfe658e64fb00d4a2ff40f35fe3b85ae8128846c
ragdoll <museopkim0214@gmail.com>
2021-03-10 02:01:02 +0900

    [#41] Feat: JWT token 반환을 위해 Security ignoring 설정 추가
    GenerationKey를 전달 받는 API 가 JwtAuthorizationFilter를 통과하지 않도록 ignoring 설정 추가

--- src/main/java/com/requestrealpiano/songrequest/config/security/SecurityConfig.java
+++ src/main/java/com/requestrealpiano/songrequest/config/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -36,6 +37,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/api/account/token");
+    }
+
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors(withDefaults());
 
@@ -45,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
             .antMatchers("/**").permitAll()
 //            .antMatchers("/", "/api/account/token").permitAll()
-//            .antMatchers("/api/**").hasAnyRole(Role.MEMBER.getValue(), Role.ADMIN.getValue())
+//            .antMatchers("/api/letters").hasAnyRole(Role.MEMBER.getValue(), Role.ADMIN.getValue())
             .anyRequest().authenticated();
 
         http.sessionManagement()
