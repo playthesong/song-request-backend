559321ec745640f202f8554858aa7e9d535595d3
ragdoll <museopkim0214@gmail.com>
2021-03-24 02:06:37 +0900

    [#50] Fix: ValidationCondition - interface -> class 변경
    - Enum으로 변경을 고려 하였으나 각 값들이 모두 같은 조건 구성을 가지지 않아 class로 변경

--- src/main/java/com/requestrealpiano/songrequest/global/constant/ValidationCondition.java
+++ src/main/java/com/requestrealpiano/songrequest/global/constant/ValidationCondition.java
@@ -1,25 +1,25 @@
 package com.requestrealpiano.songrequest.global.constant;
 
-public interface ValidationCondition {
+public class ValidationCondition {
 
     // Common
-    String NOT_EMPTY_MESSAGE = "필수 입력 정보 입니다.";
+    public static final String NOT_EMPTY_MESSAGE = "필수 입력 정보 입니다.";
 
     // Artist
-    String ARTIST_MESSAGE = "아티스트는 30자 미만 입니다.";
-    int ARTIST_MAX = 30;
-    int ARTIST_MIN = 1;
+    public static final String ARTIST_MESSAGE = "아티스트는 30자 미만 입니다.";
+    public static final int ARTIST_MAX = 30;
+    public static final int ARTIST_MIN = 1;
 
     // Title
-    String TITLE_MESSAGE = "제목은 30자 미만 입니다.";
-    int TITLE_MAX = 30;
-    int TITLE_MIN = 1;
+    public static final String TITLE_MESSAGE = "제목은 30자 미만 입니다.";
+    public static final int TITLE_MAX = 30;
+    public static final int TITLE_MIN = 1;
 
     // Image URL
-    String IMAGE_MESSAGE = "유효한 이미지 정보가 아닙니다.";
-    int IMAGE_MAX = 100;
+    public static final String IMAGE_MESSAGE = "유효한 이미지 정보가 아닙니다.";
+    public static final int IMAGE_MAX = 100;
 
     // Song Story
-    String SONG_STORY_MESSAGE = "사연은 500자 미만이어야 합니다.";
-    int SONG_STORY_MAX = 500;
+    public static final String SONG_STORY_MESSAGE = "사연은 500자 미만이어야 합니다.";
+    public static final int SONG_STORY_MAX = 500;
 }
