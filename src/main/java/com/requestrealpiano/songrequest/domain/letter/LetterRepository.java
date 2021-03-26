9190fcf154ecbe15ca70e59a3baa462bb96cc797
ragdoll <museopkim0214@gmail.com>
2021-03-26 18:55:38 +0900

    [#56] Feat: LetterRepository 신청 상태별 목록 조회 메서드 추가
    findAllByRequestStatus()
    - (+) 테스트 추가

--- src/main/java/com/requestrealpiano/songrequest/domain/letter/LetterRepository.java
+++ src/main/java/com/requestrealpiano/songrequest/domain/letter/LetterRepository.java
@@ -2,5 +2,9 @@ package com.requestrealpiano.songrequest.domain.letter;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LetterRepository extends JpaRepository<Letter, Long>  {
+
+    List<Letter> findAllByRequestStatus(RequestStatus requestStatus);
 }
--- src/test/java/com/requestrealpiano/songrequest/domain/letter/LetterRepositoryTest.java
+++ src/test/java/com/requestrealpiano/songrequest/domain/letter/LetterRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.requestrealpiano.songrequest.domain.letter;
 
 import com.requestrealpiano.songrequest.testconfig.BaseRepositoryTest;
-import com.requestrealpiano.songrequest.testobject.LetterFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +9,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import static com.requestrealpiano.songrequest.domain.letter.RequestStatus.*;
 import static com.requestrealpiano.songrequest.testobject.LetterFactory.createLetter;
 import static com.requestrealpiano.songrequest.testobject.LetterFactory.createLetters;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LetterRepositoryTest extends BaseRepositoryTest {
 
@@ -33,6 +34,29 @@ class LetterRepositoryTest extends BaseRepositoryTest {
         assertThat(savedLetters).hasSize(letters.size());
         assertThat(savedLetters.stream().filter(letter -> Optional.ofNullable(letter.getId()).isPresent())
                                .count()).isEqualTo(letters.size());
+    }
+
+    @Test
+    @DisplayName("RequestStatus를 기준으로 Letter 목록을 조회하는 테스트")
+    void find_all_by_request_status() {
+        // given
+        List<Letter> letters = createLetters();
+
+        // when
+        letterRepository.saveAll(letters);
+        List<Letter> waitingLetters = letterRepository.findAllByRequestStatus(WAITING);
+        List<Letter> pendingLetters = letterRepository.findAllByRequestStatus(PENDING);
+        List<Letter> doneLetters = letterRepository.findAllByRequestStatus(DONE);
+
+        // then
+        assertAll(
+                () -> assertThat(waitingLetters.stream()
+                                               .allMatch(letter -> letter.getRequestStatus().equals(WAITING))).isTrue(),
+                () -> assertThat(pendingLetters.stream()
+                                               .allMatch(letter -> letter.getRequestStatus().equals(PENDING))).isTrue(),
+                () -> assertThat(doneLetters.stream()
+                                            .allMatch(letter -> letter.getRequestStatus().equals(DONE))).isTrue()
+        );
     }
 
     @Test
