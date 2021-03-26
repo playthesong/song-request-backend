3338dccf9e49e87a83b8168f7d4ca2bde5df5210
ragdoll <museopkim0214@gmail.com>
2021-03-27 04:28:12 +0900

    [#58] Test: LetterRepositoryTest - 전체 Letter 목록 조회 Pagination 테스트 추가

--- src/test/java/com/requestrealpiano/songrequest/domain/letter/LetterRepositoryTest.java
+++ src/test/java/com/requestrealpiano/songrequest/domain/letter/LetterRepositoryTest.java
@@ -1,17 +1,32 @@
 package com.requestrealpiano.songrequest.domain.letter;
 
+import com.requestrealpiano.songrequest.domain.letter.request.PaginationParameters;
+import com.requestrealpiano.songrequest.global.constant.SortProperties;
 import com.requestrealpiano.songrequest.testconfig.BaseRepositoryTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static com.requestrealpiano.songrequest.domain.letter.RequestStatus.*;
-import static com.requestrealpiano.songrequest.testobject.LetterFactory.createLetter;
-import static com.requestrealpiano.songrequest.testobject.LetterFactory.createLetters;
+import static com.requestrealpiano.songrequest.global.constant.SortProperties.CREATED_DATE_TIME;
+import static com.requestrealpiano.songrequest.testobject.AccountFactory.createMember;
+import static com.requestrealpiano.songrequest.testobject.LetterFactory.*;
+import static com.requestrealpiano.songrequest.testobject.PaginationFactory.createPaginationParametersOf;
+import static com.requestrealpiano.songrequest.testobject.SongFactory.createSong;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -19,6 +34,23 @@ class LetterRepositoryTest extends BaseRepositoryTest {
 
     @Autowired
     LetterRepository letterRepository;
+
+    @ParameterizedTest
+    @MethodSource("paginationFindAllLettersParameters")
+    @DisplayName("Paging 처리된 Letter 목록 조회 테스트")
+    void pagination_find_all_letters(List<Letter> letters, Integer page, Integer size) {
+        // given
+        PaginationParameters parameters = createPaginationParametersOf(page, size);
+        PageRequest pageRequest = PageRequest.of(parameters.getPage(), parameters.getSize(),  Sort.by(Direction.DESC, CREATED_DATE_TIME.getFieldName()));
+
+        // when
+        letterRepository.saveAll(letters);
+        Page<Letter> savedLetters = letterRepository.findAll(pageRequest);
+
+        // then
+        assertThat(savedLetters.getContent().size()).isEqualTo(size);
+        assertThat(savedLetters.getTotalElements()).isEqualTo(letters.size());
+    }
 
     @Test
     @DisplayName("RequestStatus를 기준으로 Letter 목록을 조회하는 테스트")
@@ -56,5 +88,19 @@ class LetterRepositoryTest extends BaseRepositoryTest {
         // then
         assertThat(savedLetter.getCreatedDateTime()).isNotNull();
         assertThat(savedLetter.getCreatedDateTime()).isAfter(testDateTime);
+    }
+
+    private static Stream<Arguments> paginationFindAllLettersParameters() {
+        Integer page = 1;
+        Integer size = 20;
+        List<Letter> letters = new ArrayList<>();
+
+        for (int i = 0; i < 10 ; i++) {
+            letters.addAll(createLetters());
+        }
+
+        return Stream.of(
+                Arguments.of(letters, page, size)
+        );
     }
 }
