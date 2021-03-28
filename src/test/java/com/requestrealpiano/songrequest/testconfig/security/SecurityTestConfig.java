0a5c6ca442e18958a1887ec569c84daa4115b375
ragdoll <museopkim0214@gmail.com>
2021-03-29 00:19:27 +0900

    [#54] Test: Mock Authentication Filter - Security 테스트 환경에 추가
    `@AuthenticationPrincipal` 을 Mocking 하기 위해 추가

--- src/test/java/com/requestrealpiano/songrequest/testconfig/security/SecurityTestConfig.java
+++ src/test/java/com/requestrealpiano/songrequest/testconfig/security/SecurityTestConfig.java
@@ -3,6 +3,7 @@ package com.requestrealpiano.songrequest.testconfig.security;
 import com.requestrealpiano.songrequest.domain.account.Role;
 import com.requestrealpiano.songrequest.security.oauth.CustomAccessDeniedHandler;
 import com.requestrealpiano.songrequest.security.oauth.CustomAuthenticationEntryPoint;
+import com.requestrealpiano.songrequest.testconfig.security.filter.MockAuthenticationFilter;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Import;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
@@ -48,7 +50,8 @@ public class SecurityTestConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/songs").hasAnyRole(MEMBER.getKey(), ADMIN.getKey())
             .anyRequest().authenticated();
 
-        http.addFilterBefore(characterEncodingFilter(), CsrfFilter.class);
+        http.addFilterBefore(characterEncodingFilter(), CsrfFilter.class)
+            .addFilterBefore(new MockAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         http.exceptionHandling()
             .authenticationEntryPoint(customAuthenticationEntryPoint)
